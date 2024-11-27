101f00a4e94772348c55bf70c96a91fb402e1c5d
moakajsa <grka24fe@student.ju.se>
2024-11-27 23:59:14 +0100

    base

--- game.js
+++ game.js
@@ -1,63 +1,122 @@
-let character;
-let platform;
-let acceleration;
-let velocityY;
+let player;
+let platforms = [];
+let gravity = 0.8;
+let friction = 0.9;
+let jumpStrength = -12;
 
 function setup() {
-  createCanvas(800, 600);
-  background(255, 255, 255);
-  character = new Character(20, 20, 20);
-  platform = new Platform(20, 20, 20);
-  acceleration = 1.5;
-  velocityY = 10;
-}
+  createCanvas(800, 700);
 
-class Character {
-  constructor(x, y, s) {
-    this.x = x;
-    this.y = y;
-    this.s = s;
-  }
-  // falling() {
-  //   this.y = this.y + this.velocityY;
-  //   this.velocitY = this.velocityY + this.acceleration;
-  // }
-  show() {
-    push();
-    clear();
-    fill(0, 0, 0);
-    noStroke();
-    scale(this.size);
-    rect(this.x, this.y, 100, 100);
-    pop();
-  }
-  move() {
-    this.x = this.x;
-    this.y = this.y + acceleration + velocityY;
-  }
-  update() {
-    this.move();
-  }
-}
+  player = new Player();
 
-class Platform {
-  constructor(x, y, s) {
-    this.x = x;
-    this.y = y;
-    this.s = s;
-  }
-  show() {
-    push();
-    fill(0, 0, 0);
-    noStroke();
-    scale(this.size);
-    rect(this.x, this.y + 500, 300, 40, 60);
-    pop();
-  }
+  // Create platforms
+  platforms.push(new Platform(0, height - 50, width, 50)); // Ground
 }
 
 function draw() {
-  character.show();
-  character.update();
-  platform.show();
+  background(135, 206, 250); // Sky color
+
+  // Apply gravity to player
+  player.applyGravity();
+
+  // Move player
+  player.update();
+
+  // Display player
+  player.display();
+
+  // Display platforms
+  for (let platform of platforms) {
+    platform.display();
+  }
+
+  // Check for collision with platforms
+  for (let platform of platforms) {
+    player.checkCollision(platform);
+  }
+}
+
+// Player class
+class Player {
+  constructor() {
+    this.x = 50;
+    this.y = height - 150;
+    this.width = 50;
+    this.height = 50;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.onGround = false;
+  }
+
+  update() {
+    // Left and right movement
+    if (keyIsDown(LEFT_ARROW)) {
+      this.xSpeed = -5;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      this.xSpeed = 5;
+    } else {
+      this.xSpeed = this.xSpeed * friction; // Slow down when no key is pressed
+    }
+
+    this.x = this.x + this.xSpeed;
+    this.y = this.y + this.ySpeed;
+
+    // Make sure the player stays within bounds horizontally
+    this.x = constrain(this.x, 0, width - this.width);
+  }
+
+  applyGravity() {
+    if (!this.onGround) {
+      this.ySpeed = this.ySpeed + gravity; // Falling down
+    }
+  }
+
+  jump() {
+    if (this.onGround) {
+      this.ySpeed = jumpStrength;
+      this.onGround = false;
+    }
+  }
+
+  checkCollision(platform) {
+    // Check if player is colliding with the platform
+    if (
+      this.x + this.width > platform.x &&
+      this.x < platform.x + platform.width &&
+      this.y + this.height <= platform.y &&
+      this.y + this.height + this.ySpeed >= platform.y
+    ) {
+      this.ySpeed = 0;
+      this.y = platform.y - this.height; // Position player on top of the platform
+      this.onGround = true;
+    }
+  }
+
+  display() {
+    fill(255, 0, 0);
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
+
+// Platform class
+class Platform {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+  }
+
+  display() {
+    fill(0, 255, 0);
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
+
+// Key press for jump
+function keyPressed() {
+  if (keyCode === 32) {
+    // Space bar
+    player.jump();
+  }
 }
